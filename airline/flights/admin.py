b2bbf8ea4a94986115a39071ac78c0cb18b34f78
Ángel Rodríguez <angelrol0112@gmail.com>
2024-05-14 21:14:52 -0600

    Finalización de proyecto hasta sección de SQL

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from .models import Flight, Airport
+from .models import Flight, Airport, Passenger
 
 # Register your models here.
-admin.site.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")         #Quiero ver todo esto cuando cargas un vuelo
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)                                   #A la izquierda se muestran los vuelos disponibles, a la derecha los registrados 
+
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Airport)
+admin.site.register(Passenger, PassengerAdmin)
